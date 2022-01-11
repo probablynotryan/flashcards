b9ffc0664d3c70d33deb6cd2fca0dafc0b9cb709
Ryan Bass <theryanbass@gmail.com>
2022-01-11 14:53:44 -0800

    add background picture, body code, styling and script
    CoAuthored by Ryan Bass (theryanbass@gmail.com)
    CoAuthored by Chase Stokes (chasehstokes@gmail.com)

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+  $(".clickable").click(function() {
+    $(".js-showing").slideToggle();
+    $(".js-hidden").slideToggle();
+  });
+  $(".clickable1").click(function() {
+    $(".operators-showing").slideToggle();
+    $(".operators-hidden").slideToggle();
+  });
+  $(".clickable2").click(function() {
+    $(".variables-showing").slideToggle();
+    $(".variables-hidden").slideToggle();
+  });
+  $(".clickable3").click(function() {
+    $(".vName-showing").slideToggle();
+    $(".vName-hidden").slideToggle();
+  });
+  $(".clickable4").click(function() {
+    $(".functions-showing").slideToggle();
+    $(".functions-hidden").slideToggle();
+  });
+  $(".clickable5").click(function() {
+    $(".methods-showing").slideToggle();
+    $(".methods-hidden").slideToggle();
+  });
+  $(".clickable6").click(function() {
+    $(".parameters-showing").slideToggle();
+    $(".parameters-hidden").slideToggle();
+  });
+  $(".clickable7").click(function() {
+    $(".return-showing").slideToggle();
+    $(".return-hidden").slideToggle();
+  });
+  $(".clickable9").click(function() {
+    $(".chaining-showing").slideToggle();
+    $(".chaining-hidden").slideToggle();
+  });
+  $(".clickable10").click(function() {
+    $(".strings-showing").slideToggle();
+    $(".strings-hidden").slideToggle();
+  });
+  $(".clickable11").click(function() {
+    $(".booleans-showing").slideToggle();
+    $(".booleans-hidden").slideToggle();
+  });
+  $(".clickable12").click(function() {
+    $(".undefined-showing").slideToggle();
+    $(".undefined-hidden").slideToggle();
+  });
+  $(".clickable13").click(function() {
+    $(".nan-showing").slideToggle();
+    $(".nan-hidden").slideToggle();
+  });
+  $(".clickable14").click(function() {
+    $(".alert-showing").slideToggle();
+    $(".alert-hidden").slideToggle();
+  });
+  $(".clickable15").click(function() {
+    $(".comments-showing").slideToggle();
+    $(".comments-hidden").slideToggle();
+  });
+  $(".clickable16").click(function() {
+    $(".jquery-showing").slideToggle();
+    $(".jquery-hidden").slideToggle();
+  });
+  $(".clickable17").click(function() {
+    $(".attributes-showing").slideToggle();
+    $(".attributes-hidden").slideToggle();
+  });
+
+})
